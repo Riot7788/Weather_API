1de1b557b66d5a18e0630577c317c4ac2e304ee2
Aleksandr <alex19950318@gmail.com>
2025-11-19 21:19:11 +0300

    Created a new weather query model. Added a new service file and added the views extension. I have set up the admin panel.

--- src/weather_app/admin.py
+++ src/weather_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WeatherQuery
+
+
+@admin.register(WeatherQuery)
+class WeatherQueryAdmin(admin.ModelAdmin):
+    list_display = ('city', 'country', 'temperature', 'unit', 'wind_speed', 'timestamp', 'from_cache')
+    list_filter = ('from_cache', 'unit', 'timestamp', 'country')
+    search_fields = ('city', 'country', 'description')
+    readonly_fields = ('timestamp',)
+    ordering = ('-timestamp',)
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('city', 'country', 'latitude', 'longitude')
+        }),
+        ('Погодные данные', {
+            'fields': ('temperature', 'unit', 'wind_speed', 'description')
+        }),
+        ('Метаданные', {
+            'fields': ('timestamp', 'from_cache')
+        }),
+    )
