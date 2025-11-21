7fc6fc522144a79d34173cd935b4bd5b5f99d597
Aleksandr <alex19950318@gmail.com>
2025-11-21 15:06:28 +0300

    Added the ability to save weather query history to a CSV file.

--- src/weather_app/urls.py
+++ src/weather_app/urls.py
@@ -4,11 +4,13 @@ from django.urls import path
 from .views import (
     home_view,
     query_history,
+    export_history_csv,
 
 )
 
 urlpatterns = [
     path("", home_view, name="home"),
     path('query_history/', query_history, name="history"),
+    path('export/', export_history_csv, name='export_csv'),
 
 ]
